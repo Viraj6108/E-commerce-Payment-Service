cabbb5dc2b2585d3ed4ae19a7f5c34a6bd810c66
Viraj Malikwade <veer6676@gmail.com>
2025-10-29 18:27:37 +0530

    fake payment service

--- src/main/java/com/webdev/ws/handler/PaymentHandler.java
+++ src/main/java/com/webdev/ws/handler/PaymentHandler.java
@@ -5,16 +5,20 @@ import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import com.webdev.ws.errors.NotRetryableException;
+import com.webdev.ws.events.PaymentFailedEvent;
 import com.webdev.ws.events.PaymentProcessEvent;
+import com.webdev.ws.events.PaymentSuccessfulEvent;
 import com.webdev.ws.model.PaymentModel;
 import com.webdev.ws.repository.PaymentRepository;
 
@@ -22,16 +26,21 @@ import com.webdev.ws.repository.PaymentRepository;
 @KafkaListener(topics = "payment-command", groupId = "payment-group", containerFactory = "concurrentKafkaListenerContainerFactory")
 public class PaymentHandler {
 
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
 	
 	Logger logger = LoggerFactory.getLogger(this.getClass());
 	RestTemplate restTemplate;
+	@Value("${payment.successful.event}")
+	private String PAYMENT_EVENT;
 	
 	private PaymentRepository repository;
 	public PaymentHandler(RestTemplate restTemplate,
-			PaymentRepository repository)
+			PaymentRepository repository, KafkaTemplate<String, Object> kafkaTemplate)
 	{
 		this.restTemplate = restTemplate;
 		this.repository = repository;
+		this.kafkaTemplate = kafkaTemplate;
 		
 	}
 	
@@ -51,11 +60,17 @@ public class PaymentHandler {
 			model.setPaymentMethod(details.getMethod());
 			model.setTransactionId(UUID.randomUUID());
 			repository.save(model);
-			
+			PaymentSuccessfulEvent event  = new PaymentSuccessfulEvent(model.getTransactionId(),
+					command.getOrderId(),command.getProductId());
+			kafkaTemplate.send(PAYMENT_EVENT,event);
+			logger.info("Payment successfull, Sent to topic"+PAYMENT_EVENT);
 		}
 		}catch(DataAccessException e)
 		{
-			throw new NotRetryableException("Not able to update the payment try again later", e);
+			PaymentFailedEvent failedEvent = new PaymentFailedEvent(command.getProductId()
+					,command.getOrderId(),command.getQuantity());
+			kafkaTemplate.send(PAYMENT_EVENT,failedEvent);
+			throw new NotRetryableException("Not able to update the payment try again later");
 		}
 	}
 	@KafkaHandler(isDefault = true)
